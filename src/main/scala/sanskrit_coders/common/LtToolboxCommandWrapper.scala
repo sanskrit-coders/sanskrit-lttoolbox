1c52d0199580406e8a33283bcbb337fa16b230b3
vishvAsaH <vishvas.vasuki@gmail.com>
2017-07-03 13:15:18 -0700

    Add subanta Generator, remove scl_bin

--- src/main/scala/sanskrit_coders/common/LtToolboxCommandWrapper.scala
+++ src/main/scala/sanskrit_coders/common/LtToolboxCommandWrapper.scala
@@ -3,11 +3,11 @@ package sanskrit_coders.common
 import org.slf4j.LoggerFactory
 import scala.sys.process._
 
-class LtToolboxCommandWrapper {
+class LtToolboxCommandWrapper(val binFilePath: String) {
   val log = LoggerFactory.getLogger(getClass.getName)
-  def queryBin(binFilePath: String, query: String): String = {
-    val command = s"echo ${query}|/usr/bin/lt-proc -ct $binFilePath"
-    log info command
+  def queryBin(query: String): String = {
+    val command = s"echo ${query}" #| s"/usr/bin/lt-proc -ct $binFilePath"
+    log debug command.toString
     val stdout = new StringBuilder
     val stderr = new StringBuilder
     val status = command ! ProcessLogger(stdout append _, stderr append _)
@@ -15,6 +15,7 @@ class LtToolboxCommandWrapper {
       log error s"status ${status}, stderr: $stderr, stdout: $stdout"
       throw new RuntimeException(stderr.toString())
     }
+    log debug stdout.toString()
     stdout.toString()
   }
 }
---
+++ src/main/scala/sanskrit_coders/scl/Generators.scala
@@ -0,0 +1,48 @@
+package sanskrit_coders.scl
+
+import org.slf4j.{Logger, LoggerFactory}
+import sanskrit_coders.common.LtToolboxCommandWrapper
+
+class SubantaGenerator(override val binFilePath: String) extends LtToolboxCommandWrapper(binFilePath=binFilePath) {
+  override val log: Logger = LoggerFactory.getLogger(getClass.getName)
+  final val prakaaraCodeMap = Map[String, String](
+    "sAXAraNa" -> "nA",
+    "sarvanAma" -> "sarva",
+    "safkhyA" -> "saMKyA",
+    "safkhyeya" -> "saMKyeyam",
+    "pUraNa" -> "pUraNam"
+  )
+  val lingaCodeMap = Map[String, String](
+    "pum" -> "puM",
+    "napum" -> "napuM",
+    "swrI" -> "swrI"
+  )
+  val vachanaCodeMap = Map[Int, String](
+    1 -> "eka",
+    2 -> "xwi",
+    3 -> "bahu"
+  )
+
+  // Example query: jFAna<vargaH:nA><lifgam:napuM><viBakwiH:1><vacanam:bahu><level:1>
+  def getQuery(root: String, prakAra: String, linga: String, vibhakti: Int, vachana: Int) : String = {
+    s"${root}" +
+      s"<vargaH:${prakaaraCodeMap.get(prakAra).get}>" +
+      s"<lifgam:${lingaCodeMap.get(linga).get}>" +
+      s"<viBakwiH:${vibhakti}>" +
+      s"<vacanam:${vachanaCodeMap.get(vachana).get}>" +
+      s"<level:1>"
+  }
+
+  def getSubanta(root: String, prakAra: String, linga: String, vibhakti: Int, vachana: Int): String = {
+    queryBin(getQuery(root, prakAra, linga, vibhakti, vachana))
+  }
+}
+
+object subantaGeneratorTest {
+  val log = LoggerFactory.getLogger(getClass.getName)
+  def main(args: Array[String]): Unit = {
+    val subantaGenerator = new SubantaGenerator(binFilePath = "/home/vvasuki/scl/build/morph_bin/sup_gen.bin")
+    var subanta = subantaGenerator.getSubanta("jFAna", "sAXAraNa", "napum", 2, 3)
+    log info s"subanta: $subanta"
+  }
+}
